29bec19c0cd6c7a3e0544792255a645aa64ed498
Ashwin R <ashwinkey04@gmail.com>
2020-01-16 21:54:13 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# MyRecorder
+
+This is an android app which records audio and is built using Kotlin. 
+
+### Screenshots 
+
+<img src="screen.jpg" width="210">
---
+++ app/src/main/java/tk/imagingens/myrecorder/MainActivity.kt
@@ -0,0 +1,30 @@
+package tk.imagingens.myrecorder
+
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun buttonEffect(button: View) {
+        button.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.background.setColorFilter(-0x1f0b8adf, PorterDuff.Mode.SRC_ATOP)
+                    v.invalidate()
+                }
+                MotionEvent.ACTION_UP -> {
+                    v.background.clearColorFilter()
+                    v.invalidate()
+                }
+            }
+            false
+        }
+    }
+}
